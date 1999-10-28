ff1cb6eab34b38d14913cfa8fca093920490bd3d
chris <chris>
1999-10-28 22:12:29 +0000

    Just the test file

--- testissue.pl
+++ testissue.pl
@@ -4,9 +4,11 @@ use strict;
 #use DBI;
 use C4::Database;
 use C4::Circulation;
+use C4::Interface;
 
 my $num=$ARGV[0];
 
 my @data=Issue($num,3);
-print @data;
+my $data=join(' ',@data);
+resultout('console',$data);
 print "\n$data[21]\n";
