af4bda07a8710dbbbaa34abb5477056c1f26f74d
olwen <olwen>
1999-10-30 09:57:52 +0000

    Olwen additions

--- C4/Search.pm
+++ C4/Search.pm
@@ -14,7 +14,7 @@ use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 $VERSION = 0.01;
     
 @ISA = qw(Exporter);
-@EXPORT = qw(&CatSearch);
+@EXPORT = qw(&CatSearch &BornameSearch);
 %EXPORT_TAGS = ( );     # eg: TAG => [ qw!name1 name2! ],
 		  
 # your exported package globals go here,
@@ -50,14 +50,54 @@ my $priv_func = sub {
 						    
 # make all your functions, whether exported or not;
 
-
-
 sub CatSearch  {
   my ($searchstring,$type)=@_;
   my $dbh = &C4Connect;
-  my $query="Select * from catalogueentry where catalogueentry like
-  '%$searchstring%' and entrytype like '$type%'";
-  print $query,"\n";
+  my $query = '';
+  SWITCH: {
+    if ($type eq 'aa') {
+      $query="Select * from biblio,catalogueentry,biblioanalysis
+      where (catalogueentry.catalogueentry = biblioanalysis.catalogueentry)
+      and (bibliosubject.biblionumber = biblio.biblionumber)
+      and (catalogueentry.catalogueentry like '%$searchstring%') 
+      and (entrytype like '$type%')";
+      last SWITCH;
+    };
+    if ($type eq 'at') {
+      $query="Select * from biblio,catalogueentry,biblioanalysis
+      where (catalogueentry.catalogueentry = biblioanalysis.analyticaltitle)
+      and (bibliosubject.biblionumber = biblio.biblionumber)
+      and (catalogueentry.catalogueentry like '%$searchstring%') 
+      and (entrytype like '$type%')";
+      last SWITCH;
+    };
+    if ($type eq 'a') {
+      $query="Select * from biblio,catalogueentry 
+      where (catalogueentry.catalogueentry = biblio.author)
+      and (catalogueentry.catalogueentry like '%$searchstring%') 
+      and (entrytype like '$type%')";
+      last SWITCH;
+    };
+    if ($type eq 't') {
+      $query="Select * from biblio,catalogueentry
+      where (catalogueentry.catalogueentry = biblio.title)
+      and (catalogueentry.catalogueentry like '%$searchstring%')
+      and (entrytype like '$type%')";
+      last SWITCH;
+    };
+    if ($type eq 's') {
+      $query="Select * from biblio,catalogueentry,bibliosubject
+      where (catalogueentry.catalogueentry = bibliosubject.catalogueentry)
+      and (bibliosubject.biblionumber = biblio.biblionumber)
+      and (catalogueentry.catalogueentry like '%$searchstring%')
+      and (entrytype like '$type%')";
+      last SWITCH;
+    };
+    $query="Select * from catalogueentry where catalogueentry like
+    '%$searchstring%' and entrytype like '$type%'";
+    last SWITCH;
+  }
+  print "$query\n";
   my $sth=$dbh->prepare($query);
   $sth->execute;
   while (my @data=$sth->fetchrow_array){
@@ -66,7 +106,6 @@ sub CatSearch  {
   $sth->execute;
   $sth->finish;
   $dbh->disconnect;
-  
 }    
 
 
@@ -80,12 +119,12 @@ sub BornameSearch  {
   my $sth=$dbh->prepare($query);
   $sth->execute;
   while (my @data=$sth->fetchrow_array){
-    print "$data[0]\n";
+    print "$data[0] $data[2]\n";
   }
   $sth->execute;
   $sth->finish;
   $dbh->disconnect;
-  }
+}
 			    
 END { }       # module clean-up code here (global destructor)
 
