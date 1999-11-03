2bb0f2f60e93799f6fed44accce422e377ce47db
chris <chris>
1999-11-03 20:32:32 +0000

    More work on stats

--- C4/Stats.pm
+++ C4/Stats.pm
@@ -13,7 +13,7 @@ use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 $VERSION = 0.01;
 
 @ISA = qw(Exporter);
-@EXPORT = qw(&UpdateStats);
+@EXPORT = qw(&UpdateStats &statsreport);
 %EXPORT_TAGS = ( );     # eg: TAG => [ qw!name1 name2! ],
 
 # your exported package globals go here,
