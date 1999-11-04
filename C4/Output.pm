391d0b6e5bbbfcf71b8e28fad08d9a20dd1f9daf
chris <chris>
1999-11-04 02:31:33 +0000

    Html output module

---
+++ C4/Output.pm
@@ -0,0 +1,57 @@
+package C4::Output; #asummes C4/Output
+
+#package to deal with marking up output
+
+use strict;
+require Exporter;
+
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+
+# set the version for version checking
+$VERSION = 0.01;
+
+@ISA = qw(Exporter);
+@EXPORT = qw(&startpage);
+%EXPORT_TAGS = ( );     # eg: TAG => [ qw!name1 name2! ],
+
+# your exported package globals go here,
+# as well as any optionally exported functions
+
+@EXPORT_OK   = qw($Var1 %Hashit);
+
+
+# non-exported package globals go here
+use vars qw(@more $stuff);
+
+# initalize package globals, first exported ones
+
+my $Var1   = '';
+my %Hashit = ();
+
+
+# then the others (which are still accessible as $Some::Module::stuff)
+my $stuff  = '';
+my @more   = ();
+
+# all file-scoped lexicals must be created before
+# the functions below that use them.
+
+# file-private lexicals go here
+my $priv_var    = '';
+my %secret_hash = ();
+
+# here's a file-private function as a closure,
+# callable as &$priv_func;  it cannot be prototyped.
+my $priv_func = sub {
+# stuff goes here.
+  };
+   
+# make all your functions, whether exported or not;
+ 
+sub startpage{
+  my $string="hello"
+  reutrn($string);
+}
+    
+END { }       # module clean-up code here (global destructor)
+    
