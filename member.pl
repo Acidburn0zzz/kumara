b34ef413f3d44e8064e41be3c166ba7e1e568215
chris <chris>
1999-12-29 21:37:08 +0000

    File for doing confirmation

--- member.pl
+++ member.pl
@@ -13,23 +13,33 @@ my $input = new CGI;
 my $member=$input->param('member');
 
 print $input->header;
+#start the page and read in includes
 print startpage();
 print startmenu('member');
-print mkheadr(1,'Member Search');
+my @inputs=(["text","member",$member],
+            ["reset","reset","clr"]);
+print mkheadr(2,'Member Search');
+print mkformnotable("/cgi-bin/koha/member.pl",@inputs);
 print "You Searched for $member<p>";
 print mktablehdr;
-print mktablerow(8,'#99cc33','<b>Card</b>','<b>Surname</b>','<b>Firstname</b>','<b>Category</b>'
-,'<b>Address</b>','<b>OD/Issues</b>','<b>Fines</b>','<b>Notes</b>');
+print mktablerow(8,'#99cc33',bold('Card'),bold('Surname'),bold('Firstname'),bold('Category')
+,bold('Address'),bold('OD/Issues'),bold('Fines'),bold('Notes'),'/images/background-mem.gif');
 my $env;
 my ($count,$results)=BornameSearch($env,$member,'web');
 #print $count;
+my $toggle="white";
 for (my $i=0; $i < $count; $i++){
   #find out stats
   my ($od,$issue,$fines)=borrdata2($env,$results->[$i]{'borrowernumber'});
   $fines=$fines+0;
-  print mktablerow(8,'white',mklink("/cgi-bin/koha/addborrower.pl?bornum=".$results->[$i]{'borrowernumber'},$results->[$i]{'cardnumber'}),
+  if ($toggle eq 'white'){
+    $toggle="#ffffcc";
+  } else {
+    $toggle="white";
+  }
+  print mktablerow(8,$toggle,mklink("/cgi-bin/koha/memberentry.pl?bornum=".$results->[$i]{'borrowernumber'},$results->[$i]{'cardnumber'}),
   $results->[$i]{'surname'},$results->[$i]{'firstname'},
-  $results->[$i]{'categorycode'},$results->[$i]{'streetaddress'},"$od/$issue",$fines,
+  $results->[$i]{'categorycode'},$results->[$i]{'streetaddress'}." ".$results->[$i]{'city'},"$od/$issue",$fines,
   $results->[$i]{'borrowernotes'});
 }
 print mktableft;
--- memberentry.pl
+++ memberentry.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-#script to do a borrower enquiery/brin up borrower details etc
+#script to set up screen for modification of borrower details
 #written 20/12/99 by chris@katipo.co.nz
 
 use strict;
@@ -10,28 +10,16 @@ use C4::Search;
 
 
 my $input = new CGI;
-my $member=$input->param('member');
+my $member=$input->param('bornum');
 
 print $input->header;
 print startpage();
 print startmenu('member');
-print mkheadr(1,'Member Search');
-print "You Searched for $member<p>";
+print mkheadr(1,'Update Member Details');
+
+
 print mktablehdr;
-print mktablerow(8,'#99cc33','<b>Card</b>','<b>Surname</b>','<b>Firstname</b>','<b>Category</b>'
-,'<b>Address</b>','<b>OD/Issues</b>','<b>Fines</b>','<b>Notes</b>');
-my $env;
-my ($count,$results)=BornameSearch($env,$member,'web');
-#print $count;
-for (my $i=0; $i < $count; $i++){
-  #find out stats
-  my ($od,$issue,$fines)=borrdata2($env,$results->[$i]{'borrowernumber'});
-  $fines=$fines+0;
-  print mktablerow(8,'white',mklink("/cgi-bin/koha/addborrower.pl?bornum=".$results->[$i]{'borrowernumber'},$results->[$i]{'cardnumber'}),
-  $results->[$i]{'surname'},$results->[$i]{'firstname'},
-  $results->[$i]{'categorycode'},$results->[$i]{'streetaddress'},"$od/$issue",$fines,
-  $results->[$i]{'borrowernotes'});
-}
+
 print mktableft;
 print endmenu('member');
 print endpage();
