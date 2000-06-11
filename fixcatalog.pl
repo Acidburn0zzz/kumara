9acf13d2cf9abd486d0876beb476b864f23d7105
chris <chris>
2000-06-11 23:24:29 +0000

    work in progress

--- fixcatalog.pl
+++ fixcatalog.pl
@@ -5,30 +5,17 @@ use strict;
 
 my $dbh=C4Connect;
 
-my $sth=$dbh->prepare("Select * from stopwords");
+my $sth=$dbh->prepare("Select biblio.biblionumber,biblio.title from biblio,catalogueentry where catalogueentry.entrytype
+='t' and catalogueentry.catalogueentry=biblio.title limit 500");
 $sth->execute;
-my %stop;
 while (my $data=$sth->fetchrow_hashref){
-  $stop{$data->{'word'}}=$data->{'word'};
+  my $query="Update catalogueentry set biblionumber='$data->{'biblionumber'}' where catalogueentry.catalogueentry =
+  \"$data->{'title'}\" and catalogueentry.entrytype='t'";
+  my $sth2=$dbh->prepare($query);
+  $sth2->execute;
+  $sth2->finish;
 }
 $sth->finish;
-while (my $da=<STDIN>){
-  chomp $da;
-  my @temp=split('\t',$da);
-#  if ($temp[1] =~ /t/){
-    my $dat=$temp[0];
-#    print "hey";
-    while ( my ($key, $value) = each %stop) {
-      $dat=~ s/ $value / /gi;
-      $dat=~ s/ $value$//gi;
-      $dat=~ s/^$value //gi;
-    }
-    $dat=~ s/\'//g;
-    print "$temp[0]\t$temp[1]\t \t \t \t$dat\n";
-#  } else {
-#    print $da,"\n";
-#    print $temp[1],"\n";
-#  }
-}
+
 
 $dbh->disconnect;
